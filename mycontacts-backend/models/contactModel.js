e8cce95a1d5035d72d92f492640bbf8926f65cd9
sagar-joshi83 <joshhisagar9375@gmail.com>
2024-03-01 16:06:00 +0545

    validating contact schema

--- mycontacts-backend/models/contactModel.js
+++ mycontacts-backend/models/contactModel.js
@@ -1,29 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const validator = require("validator");
 
-
-const contactSchema = mongoose.Schema({
-
+const contactSchema = mongoose.Schema(
+  {
     user_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User"
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
     },
     name: {
-        type: String,
-        required: [true, "please enter contact name"]
+      type: String,
+      required: [true, "please enter contact name"],
+      trim: true,
     },
     email: {
-        type: String,
-        required: [true, "please enter the contact email"]
+      type: String,
+      required: [true, "please enter the contact email"],
+      validate: [validator.isEmail, "please enter a valid email"],
+      trim: true,
     },
     phone: {
-        type: String,
-        required: [true, "please enter contact phone number"]
-    }
-},
-    {
-        Timestamps: true,
-    }
+      type: String,
+      required: [true, "please enter contact phone number"],
+      trim: true,
+    },
+  },
+  {
+    Timestamps: true,
+  }
 );
 
-module.exports = mongoose.model('contact', contactSchema);
+module.exports = mongoose.model("contact", contactSchema);
